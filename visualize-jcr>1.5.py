e227df00f5c6ceb650272113b0453f67bcc3d140
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2018-12-09 18:59:20 -0200

    Adds a program to visualize the distribution of JCR > 1.5.

---
+++ visualize-jcr>1.5.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+import populate
+from config import subject, researchers_file, build_dir
+from visualize import boxplot
+
+
+def main():
+    # Comment the line bellow to remove the red dot indicating the performance of the subject in the following boxplot
+    # subject.update(populate.all(subject['ID Lattes'], subject['ID Scholar']))
+    # print('Red dot representing', subject["Nome"])
+
+    df = pd.read_excel(researchers_file, dtype={'ID Lattes': object})
+
+    # Métricas referentes a toda a carreira
+    metrics = ['Artigos JCR > 1,5']
+    boxplot(df, subject, metrics, 'fig-jcr>1.5.png', vert=False)
+
+if __name__ == "__main__":
+   main()
