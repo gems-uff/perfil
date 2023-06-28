eb3723696bdf3fff8613d8e835c96225f3886c01
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2023-06-28 14:22:12 -0300

    Adjusts the configuration file to the last assessment from CAPES

--- config.py
+++ config.py
@@ -22,11 +22,11 @@ from configured_reports.user_classes.artigo import Artigo
 # The order of the columns does not matter
 resources_path = os.getcwd() + os.sep + 'resources' + os.sep
 output_path = os.getcwd() + os.sep + 'output' + os.sep
-researchers_file = resources_path + 'colegiado.xlsx'
+researchers_file = resources_path + 'pgc.xlsx'
 
 # The first and last years, inclusive, for collecting metrics.
-start_year = 2020
-end_year = 2023
+start_year = 2017
+end_year = 2020
 
 
 # Tries to make each input on the database unique
