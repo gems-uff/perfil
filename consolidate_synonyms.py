2a2b69588e82105dfbb2a396a557f71627b4f76d
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2024-12-14 00:19:06 -0300

    Adição de projetos na consolidação

--- consolidate_synonyms.py
+++ consolidate_synonyms.py
@@ -1,13 +1,14 @@
 # Consolidate the synonyms of conferences and journals by merging duplicates
 
 import os
-from config import conferences_synonyms, journals_synonyms, resources_path
+from config import conferences_synonyms, journals_synonyms, projects_synonyms, resources_path
 from utils.dict_xlsx_utils import write_dict
 
 
 def main():
     write_dict(conferences_synonyms, resources_path + 'synonyms' + os.sep + 'conferences_synonyms')
     write_dict(journals_synonyms, resources_path + 'synonyms' + os.sep + 'journals_synonyms')
+    write_dict(projects_synonyms, resources_path + 'synonyms' + os.sep + 'projects_synonyms')
 
 
 if __name__ == "__main__":
